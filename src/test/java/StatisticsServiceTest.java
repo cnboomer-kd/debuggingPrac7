22c195e8ff596816c785e976f4e3aa4c8c00ed68
cnboomer <mrstrikeinfo@gmail.com>
2024-05-02 23:16:28 +0300

    CoverageTests

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,21 @@
+import org.example.StatisticsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+public class StatisticsServiceTest {
+    @Test
+    void findMaxCurrentAboveIncome() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxCurrentBeyondIncome() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 14};
+        long expected = 14;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+}
